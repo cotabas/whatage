436499b7c7bf99874ce788b863b1fa6a407f1d9c
cotabas <cotabas@gmail.com>
2023-02-26 16:24:06 -0800

    worx

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Whatage?
+
+a gnome extension to add the power usage to the top panel
+it hides if the wattage is 0.0
+
+## Installation:
+clone this repo to whatage@cptmo.dev in your gnome extensions folder
+probably: ~/.local/share/gnome-shell/extensions/
+
+```
+chmod +x nav_pwr
+
+cp nav_pwr /usr/bin/
+```
+
--- extension.js
+++ extension.js
@@ -2,34 +2,42 @@ const Main = imports.ui.main;
 const GLib = imports.gi.GLib;
 const St = imports.gi.St;
 const Mainloop = imports.mainloop
+
+//This is the command in the nav_bar bash file
+//it doesn't work to just pass it to the glib..
 const command = 'echo - | awk "{printf \"%.1f\",$(($(cat /sys/class/power_supply/BAT0/current_now) * $(cat /sys/class/power_supply/BAT0/voltage_now))) / 1000000000000 }" ; echo " W "'
+
 
 let panelButt, panButtText, timeout;
 
 function setButtonText() {
-  var [ok, out, err, exit] = GLib.spawn_command_line_sync('date');
+  var [ok, out, err, exit] = GLib.spawn_command_line_sync('nav_pwr');
   
-  panButtText.set_text(out.toString());
+
+  if (out.toString() == "0.0 W") {
+    panButtText.set_text('');
+  } else {
+    panButtText.set_text(out.toString());
+  }
   return true;
 }
 function init() {
   panelButt = new St.Bin({
-    style_class : "panel-button"
+    style_class : "panelButt"
   });
   panButtText = new St.Label({
-    style_class : "examplePanelText",
+    style_class : "panButtText",
     text : "init.."
   });
-
   panelButt.set_child(panButtText, 1);
 }
 
 function enable() {
-  Main.panel._leftBox.insert_child_at_index(panelButt, 1);
+  Main.panel._rightBox.insert_child_at_index(panelButt, 1);
   timeout = Mainloop.timeout_add_seconds(1.0, setButtonText);
 }
 
 function disable() {
   Mainloop.source_remove(timeout);
-  Main.panel._leftBox.remove_child(panelButt, 1);
+  Main.panel._rightBox.remove_child(panelButt, 1);
 }
